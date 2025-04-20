a276439413aa07381f7c4ec43bf2eb04f8585f9b
LucaJune <lucie.zmatlikova@gmail.com>
2025-04-20 21:41:57 +0200

    profiles.json

--- server/abl/profiles.js
+++ server/abl/profiles.js
@@ -1,52 +1,33 @@
-{
-    "{asid}": {
-      "profileList": ["Administrator", "Executives", "Readers", "Public"],
-      "useCaseMap": {}
-    },
-    "*": {
-      "profileList": ["Administrator", "Executives", "Readers", "Public"],
-      "useCaseMap": {
-        "item/create": {
-          "sysStateList": ["active"],
-          "profileList": ["Executives", "Readers", "Public"]
-        },
-        "item/delete": {
-          "sysStateList": ["active"],
-          "profileList": ["Executives", "Readers"]
-        },
-        "item/get": {
-          "sysStateList": ["active"],
-          "profileList": ["Executives", "Readers"]
-        },
-        "item/list": {
-          "sysStateList": ["active"],
-          "profileList": ["Executives", "Readers"]
-        },
-        "item/update": {
-          "sysStateList": ["active"],
-          "profileList": ["Executives", "Readers"]
-        },
-        "list/create": {
-          "sysStateList": ["active"],
-          "profileList": ["Executives", "Readers", "Public"]
-        },
-        "list/delete": {
-          "sysStateList": ["active"],
-          "profileList": ["Executives"]
-        },
-        "list/get": {
-          "sysStateList": ["active"],
-          "profileList": ["Executives", "Readers"]
-        },
-        "list/list": {
-          "sysStateList": ["active"],
-          "profileList": ["Executives", "Readers"]
-        },
-        "list/update": {
-          "sysStateList": ["active"],
-          "profileList": ["Executives"]
-        }
-      }
+const fs = require('fs');
+
+const profiles = {
+  Administrator: {
+    permissions: {
+      item: ["createAbl", "deleteAbl", "getAbl", "listAbl", "updateAbl"],
+      list: ["createAbl", "deleteAbl", "getAbl", "listAbl", "updateAbl"]
+    }
+  },
+  Executives: {
+    permissions: {
+      item: ["createAbl", "deleteAbl", "getAbl", "listAbl", "updateAbl"],
+      list: ["createAbl", "deleteAbl", "getAbl", "listAbl", "updateAbl"]
+    }
+  },
+  Readers: {
+    permissions: {
+      item: ["createAbl", "deleteAbl"],
+      list: []
+    }
+  },
+  Public: {
+    permissions: {
+      item: ["createAbl"],
+      list: []
     }
   }
-  
+};
+
+fs.writeFile('profiles.json', JSON.stringify(profiles, null, 4), (err) => {
+  if (err) throw err;
+  console.log('The profiles.json file has been created successfully.');
+});
